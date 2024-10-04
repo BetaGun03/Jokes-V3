7b6a130b4dd3164e13134f0edb8d8639d64d909e
BetaGun03 <jaimehedrera25@gmail.com>
2024-10-04 12:47:12 +0200

    Joke-Form

---
+++ src/app/joke-form/joke-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './joke-form.component.html',
+  styleUrl: './joke-form.component.css'
+})
+export class JokeFormComponent {
+
+}
